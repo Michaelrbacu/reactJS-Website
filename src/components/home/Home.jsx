7f96508dfe5bcd90659ef44b7e3c952ec9095cdc
michael <michaelrbacu2@gmail.com>
2023-07-26 20:27:26 -0400

    Home and Projects Page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,23 +1,127 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './HomePage.css';
 
 const Home = () => {
-  return (
-    <div className="homepage">
-      <h1>Welcome to My Website</h1>
-      <div className="apps-container">
-        <div className="apps">
-        <h1>Hello, I'm Michael Bacu</h1>
-      <h4>
-        On this website you can find some of the projects I've built, I have other projects on my github as well.
-      </h4>
-      <br></br>
-          <a href="https://github.com/Michaelrbacu">Github Profile</a>
+  const [displayText, setDisplayText] = useState("");
+  const strings = [
+    "Hello, I'm Michael Bacu",
+  ];
 
-          <a href="https://www.linkedin.com/in/michael-bacu-181a551a6/">LinkedIn Profile</a>
-                  </div>
-      </div>
+  let counter = 0;
+
+  const options = {
+    offset: 0,
+    timeout: 15,
+    iterations: 5,
+    characters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'x', '#', '%', '&', '-', '+', '_', '?', '/', '\\', '='],
+    resolveString: strings[counter],
+    element: document.querySelector('[data-target-resolver]')
+  };
+
+  const resolver = {
+    resolve: function resolve(options, callback) {
+      const resolveString = options.resolveString;
+      const characters = options.characters;
+      const offset = options.offset;
+      const partialString = resolveString.substring(0, offset);
+      const combinedOptions = Object.assign({}, options, { partialString: partialString });
+  
+      function getRandomInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+  
+      function randomCharacter(characters) {
+        return characters[getRandomInteger(0, characters.length - 1)];
+      }
+  
+      function doRandomiserEffect(options, callback) {
+        const characters = options.characters;
+        const timeout = options.timeout;
+        const element = options.element;
+        const partialString = options.partialString;
+  
+        let iterations = options.iterations;
+  
+        setTimeout(() => {
+          if (iterations >= 0) {
+            const nextOptions = Object.assign({}, options, { iterations: iterations - 1 });
+  
+            // Ensures partialString without the random character as the final state.
+            if (iterations === 0) {
+              element.textContent = partialString;
+            } else {
+              // Replaces the last character of partialString with a random character
+              element.textContent = partialString.substring(0, partialString.length - 1) + randomCharacter(characters);
+            }
+  
+            doRandomiserEffect(nextOptions, callback);
+          } else if (typeof callback === "function") {
+            callback();
+          }
+        }, options.timeout);
+      }
+  
+      function doResolverEffect(options, callback) {
+        const resolveString = options.resolveString;
+        const characters = options.characters;
+        const offset = options.offset;
+        const partialString = resolveString.substring(0, offset);
+        const combinedOptions = Object.assign({}, options, { partialString: partialString });
+  
+        doRandomiserEffect(combinedOptions, () => {
+          const nextOptions = Object.assign({}, options, { offset: offset + 1 });
+  
+          if (offset <= resolveString.length) {
+            doResolverEffect(nextOptions, callback);
+          } else if (typeof callback === "function") {
+            callback();
+          }
+        });
+      }
+  
+      doResolverEffect(combinedOptions, callback);
+    }
+  };
+  
+  
+
+  function callback() {
+    setTimeout(() => {
+      counter ++;
+      
+      if (counter >= strings.length) {
+        counter = 0;
+      }
+      
+      let nextOptions = Object.assign({}, options, {resolveString: strings[counter]});
+      resolver.resolve(nextOptions, callback);
+    }, 3000); // Adjust the timeout between quotes (in milliseconds)
+  }
+
+  useEffect(() => {
+    resolver.resolve(options, callback);
+
+    return () => {
+      // Cleanup if needed
+    };
+  }, []);
+
+  return (
+<div className="homepage">
+  <h1>Welcome to My Website</h1>
+  <div className="apps-container">
+    <div className="apps">
+      <h1 data-target-resolver></h1>
+      <h4>
+        <p data-target-resolver>On this website you can find some of the project Ive built.</p>
+      </h4>
+      <br />
+      <a href="https://github.com/Michaelrbacu">Github Profile</a>
+      <a href="https://www.linkedin.com/in/michael-bacu-181a551a6/">LinkedIn Profile</a>
     </div>
+  </div>
+</div>
+
   );
 };
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,47 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Projects.css";
 
-const projectsData = [
+const projectsData = [ 
   {
-    title: " Sorry the Board game built in JavaFX",
-    description: "https://github.com/Michaelrbacu/CPT-237",
+    title: "This website",
+    description: <a href='https://github.com/Michaelrbacu/reactJS-Website'>https://github.com/Michaelrbacu/reactJS-Website</a>,
   },
   {
-    title: "Project 2",
-    description: "",
+    title: "My Senior Project",
+    description: <a href='https://github.com/Michaelrbacu/SeniorProject'>https://github.com/Michaelrbacu/SeniorProject</a>,
   },
   {
-    title: "Project 3",
-    description: "",
+    title: "Sorry the Board game built in JavaFX",
+    description: <a href='https://github.com/Michaelrbacu/CPT-237'>https://github.com/Michaelrbacu/CPT-237</a>,
   },
+  {
+    title: "SQL assignments and projects.",
+    description: <a href='https://github.com/Michaelrbacu/IST-278'>https://github.com/Michaelrbacu/IST-278</a>,
+  },
+  {
+    title: "JS web programming",
+    description: <a href='https://github.com/Michaelrbacu/IST-239'>https://github.com/Michaelrbacu/IST-239</a>,
+  },
+  {
+    title: "Basic web programming",
+    description: <a href='https://github.com/Michaelrbacu/IST-226'>https://github.com/Michaelrbacu/IST-226</a>,
+  },
+ 
 ];
 
-const Projects = () => {
-  const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
 
-  const nextProject = () => {
-    setCurrentProjectIndex((prevIndex) =>
-      prevIndex === projectsData.length - 1 ? 0 : prevIndex + 1
+
+const Projects = () => {
+  const [currentSetIndex, setCurrentSetIndex] = useState(0);
+
+  const projectsPerSet = 3;
+  const totalSets = Math.ceil(projectsData.length / projectsPerSet);
+
+  const nextSet = () => {
+    setCurrentSetIndex((prevIndex) =>
+      prevIndex === totalSets - 1 ? 0 : prevIndex + 1
     );
+  };
+
+  const getCurrentProjects = () => {
+    const startIndex = currentSetIndex * projectsPerSet;
+    return projectsData.slice(startIndex, startIndex + projectsPerSet);
   };
 
   return (
     <div className="container">
-      <h1 className="heading">Projects I've built:</h1>
+      <h1 className="heading">Projects I've Built:</h1>
+      <h3>These include projects I built in classes and for practice.</h3>
+      <br />
 
-      {projectsData.map((project, index) => (
-        <div
-          key={index}
-          className={`project ${
-            index === currentProjectIndex ? "visible" : "invisible"
-          }`}
-        >
-          <h2>{project.title}</h2>
-          <p>{project.description}</p>
-        </div>
-      ))}
+      <div className="projects-grid">
+        {getCurrentProjects().map((project, index) => (
+          <div
+            key={index}
+            className={`project visible`}
+            style={{ animationDelay: `${index * 0.1}s` }}
+          >
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
 
-      <button onClick={nextProject}>Next Project</button>
+      <button onClick={nextSet}>Next Projects</button>
     </div>
   );
 };
