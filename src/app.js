d4ec2742a8529b62baae865ca5d0929ebcf35d24
michael <michaelrbacu2@gmail.com>
2023-07-30 20:58:34 -0400

    worked on chatbot changed other things

--- src/app.js
+++ src/app.js
@@ -6,97 +6,86 @@ import Home from "./components/home/Home";
 import BrightnessScrollBar from "./Settings.js";
 import settings from './settings.png'; 
 
-//
-
-
 const Projects = React.lazy(() => import("./components/projects/Projects"));
 const Weather = React.lazy(() => import("./components/weather/Weather"));
 const Todo = React.lazy(() => import("./components/todo/todo"));
-const Time = React.lazy(() => import("./components/maps/map"));
-
-
-
+const ChatBot = React.lazy(() => import("./components/bot/chatbot"));
 
 const App = () => {
   const location = useLocation();
   const [showBrightnessSettings, setShowBrightnessSettings] = useState(false);
   const [brightness, setBrightness] = useState(100);
-  
+
   const handleBrightnessButtonClick = () => {
     setShowBrightnessSettings(!showBrightnessSettings);
   };
-
   return (
-    <div>
-      <nav>
-
+    <div className="app-container">
+      <nav className="sidebar">
         <ul className="header">
           <li>
-                    <img
-  src={settings}
-  alt="Settings"
-  onClick={handleBrightnessButtonClick}
-  style={{
-    zIndex: 1,
-    cursor: 'pointer',
-    width: '40px', 
-    height: '40px'
-  }}
-/>
-          </li>
-
-          <li>
-            <Link className="nav-link" to="/home">Home</Link>
+            <Link className="nav-link" to="/home">
+              Home
+            </Link>
           </li>
           <li>
-            <Link className="nav-link" to="/projects">Projects</Link>
+            <Link className="nav-link" to="/projects">
+              Projects
+            </Link>
           </li>
           <li>
-            <Link className="nav-link" to="/weather">Weather App</Link>
+            <span className="nav-link">React Apps</span>
+            <ul className="sub-menu">
+              <li>
+                <Link className="nav-link" to="/weather">
+                  Weather App
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/todo">
+                  To Do List
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/chatbot">
+                ChatBot
+                </Link>
+              </li>
+            </ul>
           </li>
-          <li>
-            <Link className="nav-link" to="/todo">To Do List</Link>
-          </li>
-          <li>
-            <Link className="nav-link" to="/map">Map</Link>
-          </li>
-          <li>
-
-       
-         </li>
-         </ul>
-           
+        </ul>
       </nav>
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes location={location}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/weather" element={<Weather />} />
-          <Route path="/todo" element={<Todo />} />
-          <Route path="/map" element={<Time />} />
+
+      <div className="content-container">
+        <header>
+          <img
+            src={settings}
+            alt="Settings"
+            onClick={handleBrightnessButtonClick}
+            className="settings-icon"
+          />
+        </header>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes location={location}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/weather" element={<Weather />} />
+            <Route path="/todo" element={<Todo />} />
+            <Route path="/chatbot" element={<ChatBot />} />
 
           </Routes>
-        
+        </Suspense>
+
         <BrightnessScrollBar
           showScrollBar={showBrightnessSettings}
           brightness={brightness}
           setBrightness={setBrightness}
         />
-      </Suspense>
-
-      <div
-        className="content"
-        style={{
-          backgroundColor: `rgba(255, 255, 255, ${brightness / 100})`,
-        }}
-      >
       </div>
     </div>
   );
 };
 
-
 export default App;
-
-
---
+++ src/components/bot/chatbot.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import axios from "axios"; // Import axios for API requests
+import "./chatbot.css"; // ChatBot styles
+
+    const ChatBot = () => {
+      const [chatHistory, setChatHistory] = useState([
+        { sender: "bot", message: "Hi there! How can I help you?" },
+      ]);
+      const [userMessage, setUserMessage] = useState("");
+      const OPENAI_API_KEY = "";
+    
+      const handleUserMessage = (e) => {
+        setUserMessage(e.target.value);
+      };
+    
+      const handleSendMessage = async (e) => {
+        e.preventDefault();
+        if (userMessage.trim() !== "") {
+          // Add user message to chat history
+          setChatHistory((prevChatHistory) => [
+            ...prevChatHistory,
+            { sender: "user", message: userMessage },
+          ]);
+      
+          // Send user message to GPT-3 API
+          const botResponse = await getBotResponse(userMessage);
+      
+          // Update chat history with bot's response
+          setChatHistory((prevChatHistory) => [
+            ...prevChatHistory,
+            { sender: "bot", message: botResponse },
+          ]);
+      
+          // Clear input field
+          setUserMessage("");
+        }
+      };
+      
+    
+      const getBotResponse = async (userMessage) => {
+        const endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions";
+        const headers = {
+          Authorization: `Bearer ${OPENAI_API_KEY}`,
+          "Content-Type": "application/json",
+        };
+    
+        const data = {
+          prompt: userMessage,
+          max_tokens: 100,
+        };
+    
+        try {
+          const response = await axios.post(endpoint, data, { headers });
+          console.log("API Response:", response.data); // Debug logging
+          return response.data.choices[0].text;
+        } catch (error) {
+          console.error("Error processing the user message:", error);
+          console.log("Error response data:", error.response.data); // Log error response
+          return "Oops, something went wrong. Please try again later.";
+        }
+      };
+
+  return (
+    <div className="chatbot-container">
+      <div className="chat-history">
+        {chatHistory.map((msg, index) => (
+          <div key={index} className={`message ${msg.sender}`}>
+            {msg.message}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSendMessage} className="user-input">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={userMessage}
+          onChange={handleUserMessage}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatBot;
