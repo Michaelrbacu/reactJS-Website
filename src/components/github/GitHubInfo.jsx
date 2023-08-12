d0443015f534896653821e7b2e17747a107d4d6a
michael <michaelrbacu2@gmail.com>
2023-08-12 19:17:51 -0400

    github api changes

--- src/components/github/GitHubInfo.jsx
+++ src/components/github/GitHubInfo.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import "./github.css";
 
 const GitHubInfo = () => {
   const [repoData, setRepoData] = useState({});
@@ -15,7 +16,7 @@ const GitHubInfo = () => {
     // Fetch most recent commit
     fetch('https://api.github.com/repos/Michaelrbacu/reactJS-Website/commits', {
       headers: {
-        Authorization: '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'
+        Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`
       }
     })
     .then(response => response.json())
@@ -34,27 +35,26 @@ const GitHubInfo = () => {
     .catch(error => console.error('Error fetching commit data:', error));
 }, []);
 
-  return (
-    <div>
-      <h1>GitHub Repository Information</h1>
-      <p><strong>Repository Name:</strong> {repoData.name}</p>
-      <p><strong>Description:</strong> {repoData.description}</p>
-      <p><strong>Owner:</strong> {repoData.owner && repoData.owner.login}</p>
-      <p><strong>URL:</strong> <a href={repoData.html_url}>{repoData.html_url}</a></p>
+return (
+  <div className="github-info-container">
+    <h1 className="github-info-title">GitHub Repository Information</h1>
+    <p className="black"><strong>Repository Name:</strong> {repoData.name}</p>
+    <p className="black"><strong>Description:</strong> {repoData.description}</p>
+    <p className="black"><strong>Owner:</strong> {repoData.owner && repoData.owner.login}</p>
+    <p className="black"><strong>URL:</strong> <a href={repoData.html_url}>{repoData.html_url}</a></p>
 
-      {commitData.commit && (
-        <div>
-          <h2>Most Recent Commit</h2>
-          <p><strong>Author:</strong> {commitData.commit.author.name}</p>
-          <p><strong>Date:</strong> {commitData.commit.author.date}</p>
-          <p><strong>Message:</strong> {commitData.commit.message}</p>
-          <p><strong>Commit URL:</strong> <a href={commitData.html_url}>{commitData.html_url}</a></p>
-          <p><strong>Time Since Last Commit:</strong> {timeSinceLastCommit}</p>
+    {commitData.commit && (
+      <div>
+        <h2>Most Recent Commit</h2>
+        <p className="black"><strong>Author:</strong> {commitData.commit.author.name}</p>
+        <p className="black"><strong>Date:</strong> {commitData.commit.author.date}</p>
+        <p className="black"><strong>Commit URL:</strong> <a href={commitData.html_url}>{commitData.html_url}</a></p>
+        <p className="black"><strong>Time Since Last Commit:</strong> {timeSinceLastCommit}</p>
+      </div>
+    )}
+  </div>
+);
 
-        </div>
-      )}
-    </div>
-  );
 };
 
 export default GitHubInfo;
