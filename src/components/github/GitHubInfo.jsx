2de0ed6be828db7c77041106766aad77e72c5e06
michael <michaelrbacu2@gmail.com>
2023-08-13 00:31:47 -0400

    github page changes

--- src/components/github/GitHubInfo.jsx
+++ src/components/github/GitHubInfo.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import "./github.css";
+import './github.css';
 
 const GitHubInfo = () => {
   const [repoData, setRepoData] = useState({});
-  const [commitData, setCommitData] = useState({});
-  const [timeSinceLastCommit, setTimeSinceLastCommit] = useState('');
+  const [lastCommitDate, setLastCommitDate] = useState(new Date());
+  const [timeElapsed, setTimeElapsed] = useState(0); // Initialize timeElapsed state
 
   useEffect(() => {
     // Fetch repository information
@@ -14,47 +14,53 @@ const GitHubInfo = () => {
       .catch(error => console.error('Error fetching repository data:', error));
 
     // Fetch most recent commit
-    fetch('https://api.github.com/repos/Michaelrbacu/reactJS-Website/commits', {
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      setCommitData(data[0]);
-      const lastCommitDate = new Date(data[0].commit.author.date);
-      const now = new Date();
-      const timeDiff = now - lastCommitDate;
+    fetch('https://api.github.com/repos/Michaelrbacu/reactJS-Website/commits')
+      .then(response => response.json())
+      .then(data => {
+        if (data.length > 0) {
+          const lastCommitDate = new Date(data[0].commit.author.date);
+          setLastCommitDate(lastCommitDate);
+        }
+      })
+      .catch(error => console.error('Error fetching commit data:', error));
+  }, []);
 
-      const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+  const formatTime = (time) => {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((time % (1000 * 60)) / 1000);
+    const milliseconds = time % 1000;
 
-      setTimeSinceLastCommit(`${days} days ${hours} hours ${minutes} minutes`);
-    })
-    .catch(error => console.error('Error fetching commit data:', error));
-}, []);
+    return `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds ${milliseconds} ms`;
+  };
 
-return (
-  <div className="github-info-container">
-    <h1 className="github-info-title">GitHub Repository Information</h1>
-    <p className="black"><strong>Repository Name:</strong> {repoData.name}</p>
-    <p className="black"><strong>Description:</strong> {repoData.description}</p>
-    <p className="black"><strong>Owner:</strong> {repoData.owner && repoData.owner.login}</p>
-    <p className="black"><strong>URL:</strong> <a href={repoData.html_url}>{repoData.html_url}</a></p>
+  const updateElapsedTime = () => {
+    const now = new Date();
+    const timeDiff = now - lastCommitDate;
+    setTimeElapsed(timeDiff);
 
-    {commitData.commit && (
+    requestAnimationFrame(updateElapsedTime);
+  };
+
+  useEffect(() => {
+    updateElapsedTime();
+  }, [lastCommitDate]);
+
+  return (
+    <div className="github-info-container">
+      <h1 className="github-info-title">GitHub Repository Information</h1>
+      <p className="black"><strong>Repository Name:</strong> {repoData.name}</p>
+      <p className="black"><strong>Description:</strong> {repoData.description}</p>
+      <p className="black"><strong>Owner:</strong> {repoData.owner && repoData.owner.login}</p>
+      <p className="black"><strong>URL:</strong> <a href={repoData.html_url}>{repoData.html_url}</a></p>
+
       <div>
-        <h2>Most Recent Commit</h2>
-        <p className="black"><strong>Author:</strong> {commitData.commit.author.name}</p>
-        <p className="black"><strong>Date:</strong> {commitData.commit.author.date}</p>
-        <p className="black"><strong>Commit URL:</strong> <a href={commitData.html_url}>{commitData.html_url}</a></p>
-        <p className="black"><strong>Time Since Last Commit:</strong> {timeSinceLastCommit}</p>
+        <h2>Time Since Last Commit</h2>
+        <p className="black"><strong>Last Commit:</strong> {formatTime(timeElapsed)}</p>
       </div>
-    )}
-  </div>
-);
-
+    </div>
+  );
 };
 
 export default GitHubInfo;
