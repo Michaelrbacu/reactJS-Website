ef2b57d6d278c7d05ed37e8e8cbd7109dd50183f
michael <michaelrbacu2@gmail.com>
2023-08-13 02:03:30 -0400

    project page redesign

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,73 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import "./Projects.css";
+import projectImage1 from "./project-image-url-1.png";
+import projectImage2 from "./project-image-url-2.png";
+import projectImage3 from "./project-image-url-3.png";
+import projectImage4 from "./project-image-url-4.png";
+import projectImage5 from "./project-image-url-5.png";
+import projectImage6 from "./project-image-url-6.png";
 
-const projectsData = [ 
+const projectsData = [
   {
     title: "This website",
-    description: <a href='https://github.com/Michaelrbacu/reactJS-Website'>https://github.com/Michaelrbacu/reactJS-Website</a>,
+    description: "https://github.com/Michaelrbacu/reactJS-Website",
+    image: projectImage1 , // Update with correct path
   },
   {
     title: "My Senior Project",
-    description: <a href='https://github.com/Michaelrbacu/SeniorProject'>https://github.com/Michaelrbacu/SeniorProject</a>,
+    description: "https://github.com/Michaelrbacu/SeniorProject",
+    image: projectImage2,
   },
   {
-    title: "Sorry the Board game built in JavaFX",
-    description: <a href='https://github.com/Michaelrbacu/CPT-237'>https://github.com/Michaelrbacu/CPT-237</a>,
+    title: "Sorry the Board game built in JavaFX + Java Assignments",
+    description: "https://github.com/Michaelrbacu/CPT-237",
+    image: projectImage3,
   },
   {
     title: "SQL assignments and projects.",
-    description: <a href='https://github.com/Michaelrbacu/IST-278'>https://github.com/Michaelrbacu/IST-278</a>,
+    description: "https://github.com/Michaelrbacu/IST-278",
+    image: projectImage4,
   },
   {
     title: "JS web programming",
-    description: <a href='https://github.com/Michaelrbacu/IST-239'>https://github.com/Michaelrbacu/IST-239</a>,
+    description: "https://github.com/Michaelrbacu/IST-239",
+    image: projectImage5,
   },
   {
     title: "Basic web programming",
-    description: <a href='https://github.com/Michaelrbacu/IST-226'>https://github.com/Michaelrbacu/IST-226</a>,
+    description: "https://github.com/Michaelrbacu/IST-226",
+    image: projectImage6,
   },
- 
 ];
 
-
-
 const Projects = () => {
-  const [currentSetIndex, setCurrentSetIndex] = useState(0);
-
-  const projectsPerSet = 3;
-  const totalSets = Math.ceil(projectsData.length / projectsPerSet);
-
-  const nextSet = () => {
-    setCurrentSetIndex((prevIndex) =>
-      prevIndex === totalSets - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-  const getCurrentProjects = () => {
-    const startIndex = currentSetIndex * projectsPerSet;
-    return projectsData.slice(startIndex, startIndex + projectsPerSet);
-  };
-
   return (
-    <div className="container">
-      <h1 className="heading">Projects I've Built:</h1>
+    <div id="projects" className="container">
+      <h2 className="heading">Projects I've Built:</h2>
       <h3>These include projects I built in classes and for practice.</h3>
       <br />
 
-      <div className="projects-grid">
-        {getCurrentProjects().map((project, index) => (
+      <div className="contents">
+        {projectsData.map((project, index) => (
           <div
             key={index}
-            className={`project visible`}
+            className={`product`}
             style={{ animationDelay: `${index * 0.1}s` }}
           >
-            <h2>{project.title}</h2>
-            <p>{project.description}</p>
+            <img src={project.image} alt={`Project: ${project.title}`} />
+            <div className="text">
+              <h3>{project.title}</h3>
+              <a href={project.description}>GitHub Repository</a>
+            </div>
           </div>
         ))}
       </div>
-
-      <button onClick={nextSet}>Next Projects</button>
     </div>
   );
 };
