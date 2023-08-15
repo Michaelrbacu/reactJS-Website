08cdd06ed2e317c42a1c83ef200ea291013dc008
michael <michaelrbacu2@gmail.com>
2023-08-15 18:53:52 -0400

    to do list changes

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./todo.css";
+import trash from "./trash-icon.png"
+
 
 function Todo() {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState("");
+  const [taskName, setTaskName] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+  const [taskImportance, setTaskImportance] = useState("low"); // Default importance
 
   useEffect(() => {
     const storedTasks = localStorage.getItem("tasks");
@@ -16,49 +20,93 @@ function Todo() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
-  const handleInputChange = (event) => {
-    setNewTask(event.target.value);
+  const handleAddTask = () => {
+    if (taskName.trim() === "" || taskDescription.trim() === "") {
+      return;
+    }
+
+    const newTask = {
+      name: taskName,
+      description: taskDescription,
+      importance: taskImportance,
+      addedAt: new Date().toLocaleString(),
+      completed: false,
+    };
+
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+    setTaskName("");
+    setTaskDescription("");
+    setTaskImportance("low");
   };
 
-  const handleAddTask = () => {
-    if (newTask.trim() !== "") {
-      const updatedTasks = [...tasks, newTask];
-      setTasks(updatedTasks);
-      setNewTask("");
-    }
+  const handleCompleteTask = (index) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task, i) =>
+        i === index ? { ...task, completed: !task.completed } : task
+      )
+    );
   };
 
   const handleDeleteTask = (index) => {
-    const updatedTasks = tasks.filter((_, i) => i !== index);
-    setTasks(updatedTasks);
+    setTasks((prevTasks) => prevTasks.filter((_, i) => i !== index));
   };
 
   return (
     <div className="todo">
-      <div className="todo-content">
-        <ul className="todo-list">
-          {tasks.map((task, index) => (
-            <li key={index} className="chat-message">
-              {task}
-              <button onClick={() => handleDeleteTask(index)}>Delete</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="todo-form">
+      <div className="task-inputs">
         <input
           type="text"
-          placeholder="Type a message..."
-          value={newTask}
-          onChange={handleInputChange}
+          placeholder="Task Name"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
         />
-        <button onClick={handleAddTask}>Send</button>
+        <textarea
+          placeholder="Task Description"
+          value={taskDescription}
+          onChange={(e) => setTaskDescription(e.target.value)}
+        />
+        <select
+          value={taskImportance}
+          onChange={(e) => setTaskImportance(e.target.value)}
+        >
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+      <div className="todo-content">
+        <div className="task-grid">
+          {tasks.map((task, index) => (
+            <div
+              key={index}
+              className={`task ${task.completed ? "completed" : ""}`}
+            >
+              <div className="task-text">
+                <strong>{task.name}</strong>
+                <p style={{ color: '#000' }}>{task.description}</p>
+
+              </div>
+              <div className="task-info">
+                <span className="task-date">Added: {task.addedAt}</span>
+                <button onClick={() => handleCompleteTask(index)}>
+                  {task.completed ? "Undo" : "Complete"}
+                </button>
+              </div>
+              <img
+                src={trash}
+                alt="Delete"
+                className="task-delete"
+                style={{ width: '20px', height: '20px' }} // Adjust the width and height as needed
+                onClick={() => handleDeleteTask(index)}
+              />
+
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 }
-
-
-//change
 
 export default Todo;
